1ec35041e7d8ff3916dacf3f2cc062cb8a207fab
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-03-06 21:38:20 -0600

    added service-worker.js

---
+++ public/icons/service-worker.js
@@ -0,0 +1,87 @@
+const { response } = require("express");
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/service-worker.js",
+    "/styles.css"
+]
+
+self.addEventListener("install", function (evt) {
+    //pre_cache
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Yay! Your files were pre-cache completed!");
+            cache
+            .addAll(FILES_TO_CACHE)
+            .then((result) => {
+                //successful pre_cache
+                console.log(result);
+            })
+            .catch((err) => {
+                //error whoops
+                console.log(err);
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("old cache removed", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.ClientRectList.claim();
+});
+
+//so fetch
+self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches 
+            .open(DATA_CACHE_NAME)
+            .then((cache) => {
+                return fetch(evt.request)
+                .then((response) => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+
+                    return response;
+                })
+                .catch((err) => {
+                    return cache.match(evt.request);
+                });
+            })
+            .catch((err) => console.log(err))
+        );
+        return;
+    }
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
+});
